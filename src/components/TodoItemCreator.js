930329fbfe0f4cbdb442235127261a662dd852e2
Madhav Bhasin <manbhasin2015@gmail.com>
2020-07-23 13:44:57 +0530

    todo app using recoil js

---
+++ src/components/TodoItemCreator.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { v1 as uuid } from "uuid";
+import { todoListState } from "../store/rootStore";
+
+function TodoItemCreator() {
+  const [inputValue, setInputValue] = useState("");
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: uuid(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue("");
+  };
+
+  const onChange = ({ target: { value } }) => {
+    setInputValue(value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+}
+
+export default TodoItemCreator;
